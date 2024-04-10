75eb7aaffbb7ea813b85e9135686a11a029ba6f8
changchuntzu0618 <chun-tzu.chang@epfl.ch>
2024-04-10 21:40:44 +0200

    add Maptest

--- app/src/main/java/com/github/se/stepquest/map/Map.kt
+++ app/src/main/java/com/github/se/stepquest/map/Map.kt
@@ -3,11 +3,12 @@ package com.github.se.stepquest.map
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import com.google.maps.android.compose.GoogleMap
 
 @Composable
 fun Map() {
   GoogleMap(
-      modifier = Modifier.fillMaxSize(),
+      modifier = Modifier.fillMaxSize().testTag("GoogleMap"),
   ) {}
 }
