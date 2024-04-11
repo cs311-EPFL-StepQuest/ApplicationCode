f15278cae135dd61e76b5ce4eadaa74e47a22f3e
Eliott Bell <liototo.bell@gmail.com>
2024-04-11 10:28:46 +0200

    Updated tests

--- app/src/androidTest/java/com/github/se/stepquest/LoginScreenTest.kt
+++ app/src/androidTest/java/com/github/se/stepquest/LoginScreenTest.kt
@@ -2,6 +2,8 @@ package com.github.se.stepquest
 
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.compose.rememberNavController
+import com.github.se.stepquest.ui.navigation.NavigationActions
 import com.github.se.stepquest.ui.theme.StepQuestTheme
 import org.junit.Rule
 import org.junit.Test
@@ -12,7 +14,10 @@ class LoginScreenTest {
 
   @Test
   fun everythingIsDisplayed() {
-    composeTestRule.setContent { StepQuestTheme { LoginPage() } }
+
+    composeTestRule.setContent {
+      StepQuestTheme { LoginPage(NavigationActions(rememberNavController())) }
+    }
 
     // Application logo is displayed
     composeTestRule.onNodeWithTag("App logo").assertIsDisplayed()
--- app/src/main/java/com/github/se/stepquest/MainActivity.kt
+++ app/src/main/java/com/github/se/stepquest/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -28,6 +29,7 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -72,8 +74,6 @@ fun MyAppNavHost(
 
 @Composable
 fun LoginPage(navigationActions: NavigationActions) {
-  var username by remember { mutableStateOf("") }
-  var password by remember { mutableStateOf("") }
   val blueThemeColor = colorResource(id = R.color.blueTheme)
 
   fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
