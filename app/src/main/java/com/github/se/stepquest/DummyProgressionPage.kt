400c514ba0c7f455d66f71094cfbe13f3d5a395b
Santboneless <67575517+Santboneless@users.noreply.github.com>
2024-04-11 11:12:42 +0200

    Formatting correction

--- app/src/main/java/com/github/se/stepquest/DummyProgressionPage.kt
+++ app/src/main/java/com/github/se/stepquest/DummyProgressionPage.kt
@@ -30,56 +30,47 @@ import com.github.se.stepquest.ui.theme.StepQuestTheme
 
 @Composable
 fun ProgressionPageLayout() {
-    val blueThemeColor = colorResource(id = R.color.blueTheme)
-    var showDialog by remember { mutableStateOf(false) }
-    var dailyStepGoal by remember { mutableIntStateOf(5000) }
-    var weeklyStepGoal by remember { mutableIntStateOf(35000) }
+  val blueThemeColor = colorResource(id = R.color.blueTheme)
+  var showDialog by remember { mutableStateOf(false) }
+  var dailyStepGoal by remember { mutableIntStateOf(5000) }
+  var weeklyStepGoal by remember { mutableIntStateOf(35000) }
 
-    Column(
-        modifier = Modifier
-            .padding(32.dp)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(64.dp)) {
+  Column(
+      modifier = Modifier.padding(32.dp).fillMaxSize(),
+      horizontalAlignment = Alignment.CenterHorizontally,
+      verticalArrangement = Arrangement.spacedBy(64.dp)) {
         // Temporary until we have a real character
         Image(
             painter = painterResource(id = R.drawable.character),
             contentDescription = "tempCharacter",
-            modifier = Modifier
-                .width(200.dp)
-                .height(200.dp)
-        )
+            modifier = Modifier.width(200.dp).height(200.dp))
         Text(text = "Daily Steps : 0/$dailyStepGoal")
         Text(text = "Weekly Steps : 0/$weeklyStepGoal")
         Button(
             onClick = { showDialog = true },
             colors = ButtonDefaults.buttonColors(blueThemeColor),
             modifier =
-            Modifier
-                .fillMaxWidth()
-                .height(72.dp)
-                .padding(vertical = 8.dp)
-                .padding(horizontal = 16.dp),
-            shape = RoundedCornerShape(8.dp)
-        ) {
-            Text(text = "Set new step goals", color = Color.White, fontSize = 24.sp)
-        }
+                Modifier.fillMaxWidth()
+                    .height(72.dp)
+                    .padding(vertical = 8.dp)
+                    .padding(horizontal = 16.dp),
+            shape = RoundedCornerShape(8.dp)) {
+              Text(text = "Set new step goals", color = Color.White, fontSize = 24.sp)
+            }
         if (showDialog) {
-            SetStepGoalsDialog(
-                onDismiss = { showDialog = false },
-                onConfirm = { newDailyStepGoal, newWeeklyStepGoal ->
-                    dailyStepGoal = newDailyStepGoal
-                    weeklyStepGoal = newWeeklyStepGoal
-                    showDialog = false
-                }
-            )
+          SetStepGoalsDialog(
+              onDismiss = { showDialog = false },
+              onConfirm = { newDailyStepGoal, newWeeklyStepGoal ->
+                dailyStepGoal = newDailyStepGoal
+                weeklyStepGoal = newWeeklyStepGoal
+                showDialog = false
+              })
         }
-    }
+      }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ProgressionPagePreview() {
-    StepQuestTheme { ProgressionPageLayout() }
+  StepQuestTheme { ProgressionPageLayout() }
 }
-
