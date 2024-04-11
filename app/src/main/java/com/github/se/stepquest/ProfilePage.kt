9dd04476a9e1e50568a296f0bfabb381e3af8fa3
Santboneless <67575517+Santboneless@users.noreply.github.com>
2024-04-11 10:54:02 +0200

    save changes push

--- app/src/main/java/com/github/se/stepquest/ProfilePage.kt
+++ app/src/main/java/com/github/se/stepquest/ProfilePage.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -19,7 +20,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
@@ -28,8 +28,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
 import com.github.se.stepquest.ui.theme.StepQuestTheme
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
@@ -108,10 +106,12 @@ fun ProfilePageLayout() {
                 // Handle click action for Friends List button
             },
             colors = ButtonDefaults.buttonColors(blueThemeColor),
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .height(60.dp)
-                .fillMaxWidth()
+            modifier =
+            Modifier.fillMaxWidth()
+                .height(72.dp)
+                .padding(vertical = 8.dp)
+                .padding(horizontal = 16.dp),
+            shape = RoundedCornerShape(8.dp)
         ) {
             Text(
                 text = "Friends List",
