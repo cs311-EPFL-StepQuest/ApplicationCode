c6ae5f67bef073ab843e1e6d41f9e01f47ea79e9
changchuntzu0618 <chun-tzu.chang@epfl.ch>
2024-04-11 09:20:30 +0200

    update maptest

--- app/src/androidTest/java/com/github/se/stepquest/map/MapKtTest.kt
+++ app/src/androidTest/java/com/github/se/stepquest/map/MapKtTest.kt
@@ -1,8 +1,13 @@
 package com.github.se.stepquest.map
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
 import org.junit.Rule
@@ -12,14 +17,15 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MapKtTest {
 
-  @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule
+  val composeTestRule = createComposeRule()
 
   @Test
   fun map() {
-    composeTestRule.setContent { Map() }
+    composeTestRule.setContent {
+      Map()
+    }
     // Verify that the GoogleMap composable is present
     composeTestRule.onNodeWithTag("GoogleMap").assertExists()
-    // Check if GoogleMap composable is displayed
-    composeTestRule.onNodeWithTag("GoogleMap").assertIsDisplayed()
   }
 }
--- app/src/main/java/com/github/se/stepquest/map/Map.kt
+++ app/src/main/java/com/github/se/stepquest/map/Map.kt
@@ -1,6 +1,7 @@
 package com.github.se.stepquest.map
 // Map.kt
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -8,7 +9,5 @@ import com.google.maps.android.compose.GoogleMap
 
 @Composable
 fun Map() {
-  GoogleMap(
-      modifier = Modifier.fillMaxSize().testTag("GoogleMap"),
-  ) {}
+  GoogleMap(modifier = Modifier.fillMaxSize().testTag("GoogleMap"),)
 }
