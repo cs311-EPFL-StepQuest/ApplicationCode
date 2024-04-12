3ea241e138fdc0b08e09fa55195037e537b66f58
changchuntzu0618 <chun-tzu.chang@epfl.ch>
2024-04-12 16:59:30 +0200

    udpate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -160,7 +160,7 @@ dependencies {
     androidTestImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
 
     testImplementation("org.mockito.kotlin:mockito-kotlin: 3.2.0")
-    testImplementation("androidx.test:core: 1.4.0")
+//    testImplementation("androidx.test:core: 1.4.0")
     testImplementation("junit:junit: 4.13.2")
 
 
