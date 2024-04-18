48caa6d733472932948c0522589b2317a28a33dd
Santboneless <67575517+Santboneless@users.noreply.github.com>
2024-04-18 11:28:17 +0200

    Formating

--- app/src/androidTest/java/com/github/se/stepquest/ProfilePageKtTest.kt
+++ app/src/androidTest/java/com/github/se/stepquest/ProfilePageKtTest.kt
@@ -1,13 +1,6 @@
 package com.github.se.stepquest
 
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onNodeWithText
-import com.github.se.stepquest.ui.theme.StepQuestTheme
 import org.junit.Assert.*
-import org.junit.Rule
-import org.junit.Test
 
 class ProfilePageKtTest {
   /*@get:Rule val composeTestRule = createComposeRule()
