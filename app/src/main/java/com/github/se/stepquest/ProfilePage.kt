651e5e32554f1cb059242b7e036fc3b62f9ff8ed
Santboneless <67575517+Santboneless@users.noreply.github.com>
2024-04-29 13:41:28 +0200

    Test coverage for profile page

--- app/src/main/java/com/github/se/stepquest/ProfilePage.kt
+++ app/src/main/java/com/github/se/stepquest/ProfilePage.kt
@@ -35,11 +35,9 @@ import coil.compose.rememberAsyncImagePainter
 import com.github.se.stepquest.ui.navigation.NavigationActions
 import com.github.se.stepquest.ui.navigation.TopLevelDestination
 import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 
