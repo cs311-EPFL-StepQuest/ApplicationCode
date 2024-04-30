655b4b8e9ba6d6ee05a85c87842cc64f13977954
Simone <simone.andreani@epfl.ch>
2024-04-30 22:36:08 +0200

    Improve coverage (again :/)

--- app/src/androidTest/java/com/github/se/stepquest/map/MapTest.kt
+++ app/src/androidTest/java/com/github/se/stepquest/map/MapTest.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import androidx.core.content.PermissionChecker
 import androidx.lifecycle.MutableLiveData
 import androidx.test.core.app.ApplicationProvider
@@ -30,6 +31,7 @@ import io.mockk.just
 import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.mockk.verify
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import org.junit.Before
 import org.junit.Rule
@@ -254,5 +256,19 @@ class MapTest {
       googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID)
       googleMap.uiSettings.isZoomControlsEnabled = true
     }
+  }
+
+  @Test
+  fun testNumCheckpointsIncreasedAfterCreatingCheckpoint() {
+    var numCheckpoints = 0
+    composeTestRule.setContent { Map(vm).apply { numCheckpoints += 1 } }
+
+    // Simulate the user interaction to create a checkpoint
+    composeTestRule.onNodeWithContentDescription("Add checkpoint").performClick()
+    composeTestRule.onNodeWithText("Name:").performTextInput("Test")
+    composeTestRule.onNodeWithText("Confirm").performClick()
+
+    // Assert that numCheckpoints is increased by 1
+    assertEquals(numCheckpoints, 1)
   }
 }
