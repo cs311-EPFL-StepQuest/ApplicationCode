01532e6a24fd0f67d2134cc4a62ee81a5631e521
Simone <simone.andreani@epfl.ch>
2024-05-02 01:09:34 +0200

    New function to draw the route in the area

--- app/src/main/java/com/github/se/stepquest/map/LocationArea.kt
+++ app/src/main/java/com/github/se/stepquest/map/LocationArea.kt
@@ -1,47 +1,54 @@
 package com.github.se.stepquest.map
 
+import android.util.Log
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.getValue
 import kotlin.math.abs
 import kotlin.math.cos
 import kotlin.math.sqrt
 
-class LocationArea(
-    var center: LatLng,
-    var radius: Double,
-) {
+class LocationArea{
   var firebaseAuth: FirebaseAuth
   var database: FirebaseDatabase
+  private lateinit var center: LatLng
+  private var radius: Double = 1000.0
 
   init {
     firebaseAuth = FirebaseAuth.getInstance()
     database = FirebaseDatabase.getInstance()
   }
 
-  fun createArea(centerLocation: LocationDetails) {
-    this.radius = 1000.0
+  fun createArea(centerLocation: LocationDetails, radius: Double = 1000.0) {
+    this.radius = radius
     this.center = LatLng(centerLocation.latitude, centerLocation.longitude)
   }
 
   fun routesAroundLocation(
       googleMap: GoogleMap,
       selectedLocation: LocationDetails
-  ): List<StoreRoute.Route> {
+  ): List<LocationDetails> {
     // Retrieve all routes from the database and check
     // if the starting point of the route is within the circle
     val routes = database.reference.child("routes")
-    val routeList = mutableListOf<StoreRoute.Route>()
+    val routeList = mutableListOf<LocationDetails>()
 
     routes.get().addOnSuccessListener { snapshot ->
-      for (route in snapshot.children) {
-        val routeData = route.getValue(StoreRoute.Route::class.java)
-        if (routeData != null) {
-          if (checkInsideArea(routeData.route?.get(0)!!)) {
+      for (routeID in snapshot.children) {
+        val routeDataSnapshot = routeID.child("route").child("0")
+        val latitude = routeDataSnapshot.child("latitude").getValue<Double>()
+        val longitude = routeDataSnapshot.child("longitude").getValue<Double>()
+        if (latitude != null && longitude != null) {
+          val routeData = LocationDetails(latitude, longitude)
+          if (checkInsideArea(routeData)) {
             routeList.add(routeData)
           }
         }
+
       }
     }
 
@@ -54,5 +61,17 @@ class LocationArea(
     val dx = abs(center.longitude - newLocation.longitude) * kx
     val dy = abs(center.latitude - newLocation.latitude) * 111
     return sqrt(dx * dx + dy * dy) <= km
+  }
+
+  fun drawRoutesOnMap(googleMap: GoogleMap, selectedLocation: LocationDetails) {
+    val routes = routesAroundLocation(googleMap, selectedLocation)
+    for (route in routes) {
+      googleMap.addMarker(
+          MarkerOptions()
+              .position(LatLng(route.latitude, route.longitude))
+              .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+              .title("Route")
+      )
+    }
   }
 }
