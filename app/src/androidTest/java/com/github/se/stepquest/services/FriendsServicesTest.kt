300bf948250a9e8969df458cb373f489ec1b85f2
Liototo <74362833+Liototo@users.noreply.github.com>
2024-05-02 14:51:52 +0200

    Update tests

--- app/src/androidTest/java/com/github/se/stepquest/services/FriendsServicesTest.kt
+++ app/src/androidTest/java/com/github/se/stepquest/services/FriendsServicesTest.kt
@@ -1,14 +1,14 @@
 package com.github.se.stepquest.services
 
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.getValue
 import io.mockk.every
-import io.mockk.junit4.MockKRule
 import io.mockk.mockk
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,22 +16,16 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class FriendsServicesTest {
 
-  @get:Rule val mockkRule = MockKRule(this)
+  @get:Rule val composeTestRule = createComposeRule()
 
-  private lateinit var firebaseAuth: FirebaseAuth
   private lateinit var database: FirebaseDatabase
   private lateinit var requestsListRef: DatabaseReference
-  private lateinit var user: FirebaseUser
 
-  @Test
-  fun testDeletePendingRequest() {
+  @Before
+  fun setup() {
 
-    firebaseAuth = mockk()
     database = mockk(relaxed = true)
     requestsListRef = mockk(relaxed = true)
-    user = mockk()
-
-    every { firebaseAuth.currentUser } returns user
 
     every { database.reference } returns
         mockk {
@@ -41,6 +35,21 @@ class FriendsServicesTest {
                     mockk { every { child(any()) } returns requestsListRef }
               }
         }
+  }
+
+  @Test
+  fun testDeletePendingRequest() {
+
+    val friendsList = mutableListOf("friendName")
+
+    every { requestsListRef.addListenerForSingleValueEvent(any()) } answers
+        {
+          val listener = arg<ValueEventListener>(0)
+          listener.onDataChange(
+              mockk { every { getValue<List<String>>()?.toMutableList() } returns friendsList })
+        }
+
+    sendFriendRequest("friendName", "testUserId")
 
     deletePendingFriendRequest("friendName", database, "testUserId")
   }
