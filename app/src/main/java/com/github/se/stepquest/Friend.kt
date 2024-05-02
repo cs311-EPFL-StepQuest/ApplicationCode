f4cb24519edb0f86aa5789c21f181f094979edcf
Liototo <74362833+Liototo@users.noreply.github.com>
2024-05-02 17:38:14 +0200

    Fix conflicts

--- app/src/main/java/com/github/se/stepquest/Friend.kt
+++ app/src/main/java/com/github/se/stepquest/Friend.kt
@@ -1,7 +1,7 @@
 package com.github.se.stepquest
 
 data class Friend(
-    val name: String = "",
-    val profilePictureUrl: String = "",
-    val status: Boolean = false
+    val name: String,
+    val profilePictureUrl: String,
+    val status: Boolean
 )
