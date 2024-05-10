1fbc7c03775e005f03b7975ca76f7bf0d6cce638
Raytimz <teplouhov.mitya@yandex.ru>
2024-05-10 10:28:54 +0200

    REGULAR_STEP_CHALLENGE and DAILY_STEP_CHALLENGE completion functions

--- app/src/main/java/com/github/se/stepquest/UserRepository.kt
+++ app/src/main/java/com/github/se/stepquest/UserRepository.kt
@@ -12,6 +12,7 @@ interface UserRepository {
   fun getUid(): String?
 
   fun getSteps(callback: (List<Int>) -> Unit)
+  fun getDailyStepsForDate(date: Date, callback: (Int) -> Unit)
 }
 
 class IUserRepository : UserRepository {
@@ -43,6 +44,24 @@ class IUserRepository : UserRepository {
           }
         })
   }
+
+  override fun getDailyStepsForDate(date: Date, callback: (Int) -> Unit) {
+    var dailySteps = 0
+    val d = date
+    val s: CharSequence = DateFormat.format("MMMM d, yyyy ", d.time)
+    val stepsRef = database.reference.child("users").child(userId!!).child("dailySteps $s")
+    stepsRef.addListenerForSingleValueEvent(
+      object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+          dailySteps = dataSnapshot.getValue(Int::class.java) ?: 0
+          callback(dailySteps)
+        }
+
+        override fun onCancelled(databaseError: DatabaseError) {
+          callback(dailySteps)
+        }
+      })
+  }
 }
 
 class TestUserRepository1 : UserRepository {
@@ -57,6 +76,9 @@ class TestUserRepository1 : UserRepository {
   override fun getSteps(callback: (List<Int>) -> Unit) {
     return callback(listOf(dailyStepsMade, weeklyStepsMade))
   }
+  override fun getDailyStepsForDate(date: Date, callback: (Int) -> Unit) {
+    return callback(dailyStepsMade)
+  }
 }
 
 class TestUserRepository2 : UserRepository {
@@ -70,5 +92,9 @@ class TestUserRepository2 : UserRepository {
 
   override fun getSteps(callback: (List<Int>) -> Unit) {
     return callback(listOf(dailyStepsMade, weeklyStepsMade))
+  }
+
+  override fun getDailyStepsForDate(date: Date, callback: (Int) -> Unit) {
+    return callback(dailyStepsMade)
   }
 }
--- app/src/main/java/com/github/se/stepquest/data/model/ChallengeData.kt
+++ app/src/main/java/com/github/se/stepquest/data/model/ChallengeData.kt
@@ -1,16 +1,23 @@
 package com.github.se.stepquest.data.model
 
+import com.github.se.stepquest.IUserRepository
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.Date
+
+
 data class ChallengeData(
     var uuid: String = "",
-    var type: ChallengeType,
+    var type: String = "",
     var stepsToMake: Int = 0,
     var kilometersToWalk: Int = 0,
     var daysToComplete: Int = 0,
     var dateTime: String = "",
     var challengedUserUuid: String = "",
     var senderUserUuid: String = ""
-) {}
+)
 
+val userRepository = IUserRepository()
 enum class ChallengeType(
     type: String,
     messageText: String,
@@ -20,12 +27,29 @@ enum class ChallengeType(
       "REGULAR_STEP_CHALLENGE",
       "",
       fun(data: ChallengeData): Boolean {
+          val date = LocalDate.parse(data.dateTime)
+          var dailySteps = 0
+          var totalSteps = 0
+          for (i in 0..data.daysToComplete) {
+              val dateOffset = date.plusDays(i.toLong())
+              userRepository.getDailyStepsForDate(asDate(dateOffset), { steps -> dailySteps = steps })
+              totalSteps += dailySteps
+              if (totalSteps >= data.stepsToMake) return true
+          }
+          return false
         return false
       }),
   DAILY_STEP_CHALLENGE(
       "DAILY_STEP_CHALLENGE",
       "",
       fun(data: ChallengeData): Boolean {
+        val date = LocalDate.parse(data.dateTime)
+        var dailySteps = 0
+          for (i in 0..data.daysToComplete) {
+              val dateOffset = date.plusDays(i.toLong())
+              userRepository.getDailyStepsForDate(asDate(dateOffset), { steps -> dailySteps = steps })
+             if (dailySteps >= data.stepsToMake) return true
+          }
         return false
       }),
   ROUTE_CHALLENGE(
@@ -34,4 +58,8 @@ enum class ChallengeType(
       fun(data: ChallengeData): Boolean {
         return false
       })
+}
+
+private fun asDate(localDate: LocalDate): Date {
+    return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant())
 }
