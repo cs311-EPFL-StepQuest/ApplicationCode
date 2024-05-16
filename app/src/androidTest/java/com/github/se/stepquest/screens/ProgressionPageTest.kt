43b48ff6f81499b16821fef59d85be75e7e68dfb
Santboneless <67575517+Santboneless@users.noreply.github.com>
2024-05-16 10:01:06 +0200

    Cache stepgoals works on emulator

--- app/src/androidTest/java/com/github/se/stepquest/screens/ProgressionPageTest.kt
+++ app/src/androidTest/java/com/github/se/stepquest/screens/ProgressionPageTest.kt
@@ -1,21 +1,32 @@
 package com.github.se.stepquest.screens
 
+import android.content.Context
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.core.app.ApplicationProvider
 import com.github.se.stepquest.ProgressionPage
 import com.github.se.stepquest.TestUserRepository1
 import com.github.se.stepquest.TestUserRepository2
 import io.mockk.mockk
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class ProgressionPageTest {
+
+  private lateinit var context: Context
+
   @get:Rule val composeTestRule = createComposeRule()
+
+  @Before
+  fun setup() {
+    context = ApplicationProvider.getApplicationContext<Context>()
+  }
 
   @Test
   fun everythingIsDisplayed() {
-    composeTestRule.setContent { ProgressionPage(TestUserRepository1(), mockk()) }
+    composeTestRule.setContent { ProgressionPage(TestUserRepository1(), context) }
     composeTestRule.onNodeWithTag("CharacterImage").assertIsDisplayed()
     composeTestRule.onNodeWithTag("Daily steps icon").assertIsDisplayed()
     composeTestRule.onNodeWithTag("Daily steps text").assertIsDisplayed()
@@ -30,7 +41,7 @@ class ProgressionPageTest {
 
   @Test
   fun dailyGoalAchievementTest() {
-    composeTestRule.setContent { ProgressionPage(TestUserRepository2(), mockk()) }
+    composeTestRule.setContent { ProgressionPage(TestUserRepository2(), context) }
     composeTestRule.onNodeWithTag("Achieved goal message").assertIsDisplayed()
     composeTestRule.onNodeWithTag("Confirm button").assertIsDisplayed()
   }
