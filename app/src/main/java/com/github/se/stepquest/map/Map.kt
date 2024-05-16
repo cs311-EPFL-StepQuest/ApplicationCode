b6bbeba81f592dfeb4de0f8d7e9ced10324d0fdb
NizarBenmo <benmo.nizar@gmail.com>
2024-05-16 19:52:35 +0200

    Format

--- app/src/main/java/com/github/se/stepquest/map/Map.kt
+++ app/src/main/java/com/github/se/stepquest/map/Map.kt
@@ -141,8 +141,7 @@ fun Map(locationViewModel: LocationViewModel) {
 
   var currentMarker: Marker? by remember { mutableStateOf(null) }
 
-
-    val keyboardController = LocalSoftwareKeyboardController.current
+  val keyboardController = LocalSoftwareKeyboardController.current
   Scaffold(
       content = {
         Box(modifier = Modifier.fillMaxSize().testTag("MapScreen")) {
@@ -465,25 +464,24 @@ fun Map(locationViewModel: LocationViewModel) {
     while (true) {
       if (map.value != null && locationViewModel.currentLocation.value != null) {
         val coordinates =
-          LatLng(
-            locationViewModel.currentLocation.value!!.latitude,
-            locationViewModel.currentLocation.value!!.longitude)
+            LatLng(
+                locationViewModel.currentLocation.value!!.latitude,
+                locationViewModel.currentLocation.value!!.longitude)
 
-        if(currentMarker == null) {
-            val customIcon =
-                BitmapFactory.decodeResource(context.resources, R.drawable.location_dot)
-            val customIconScaled = Bitmap.createScaledBitmap(customIcon, 320, 320, false)
-            val icon = BitmapDescriptorFactory.fromBitmap(customIconScaled)
+        if (currentMarker == null) {
+          val customIcon = BitmapFactory.decodeResource(context.resources, R.drawable.location_dot)
+          val customIconScaled = Bitmap.createScaledBitmap(customIcon, 320, 320, false)
+          val icon = BitmapDescriptorFactory.fromBitmap(customIconScaled)
 
-            currentMarker = map.value!!.addMarker(
-                MarkerOptions()
-                    .position(coordinates)
-                    .anchor(0.5f, 0.5f)
-                    .icon(icon)
-                    .title("Current location marker")
-            )
+          currentMarker =
+              map.value!!.addMarker(
+                  MarkerOptions()
+                      .position(coordinates)
+                      .anchor(0.5f, 0.5f)
+                      .icon(icon)
+                      .title("Current location marker"))
         } else {
-            currentMarker!!.position = coordinates
+          currentMarker!!.position = coordinates
         }
       }
       delay(100)
