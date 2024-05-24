0f8006a12a5465c3db86d0a1895e356776f21037
Raytimz <teplouhov.mitya@yandex.ru>
2024-05-24 10:48:21 +0200

    Added some challenge tests

--- app/src/androidTest/java/com/github/se/stepquest/ChallengeTest.kt
+++ app/src/androidTest/java/com/github/se/stepquest/ChallengeTest.kt
@@ -1,9 +1,19 @@
 package com.github.se.stepquest
 
+import com.github.se.stepquest.data.model.ChallengeType
+import com.github.se.stepquest.services.createChallengeItem
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ChallengeTest {
-  @Test fun testCompletionFunctions() {}
+  @Test fun testCompletionFunctions() {
+    var challenge = createChallengeItem("testUserId", "testUserName", "testUserId", "testUserName", ChallengeType.REGULAR_STEP_CHALLENGE)
+    var result = ChallengeType.REGULAR_STEP_CHALLENGE.completionFunction(challenge)
+    assertEquals(false, result)
+    challenge = createChallengeItem("testUserId", "testUserName", "testUserId", "testUserName", ChallengeType.DAILY_STEP_CHALLENGE)
+    result = ChallengeType.DAILY_STEP_CHALLENGE.completionFunction(challenge)
+    assertEquals(false, result)
+  }
 
   @Test fun testDeleteChallenge() {}
 }
