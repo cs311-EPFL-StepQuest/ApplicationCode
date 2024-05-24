b5157bb11fbcc08b09b742ead14c6e351f68bb88
PC-DMITRY\admin <teploukhov.ds@gmail.com>
2024-05-25 00:53:27 +0200

    Added more tests for Challenge system

--- app/src/androidTest/java/com/github/se/stepquest/ChallengeTest.kt
+++ app/src/androidTest/java/com/github/se/stepquest/ChallengeTest.kt
@@ -1,11 +1,43 @@
 package com.github.se.stepquest
 
+import com.github.se.stepquest.data.model.ChallengeData
+import com.github.se.stepquest.data.model.ChallengeProgression
 import com.github.se.stepquest.data.model.ChallengeType
 import com.github.se.stepquest.services.createChallengeItem
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ChallengeTest {
+
+  @Test
+  fun challengeDataIsCorrect() {
+    val challenge =
+        ChallengeData(
+            "testUuid",
+            ChallengeType.REGULAR_STEP_CHALLENGE,
+            1000,
+            10,
+            7,
+            "2021-01-01",
+            "testChallengedUsername",
+            "testChallengedUserUuid",
+            "testSenderUsername",
+            "testSenderUserUuid",
+            ChallengeProgression("testChallengedUserUuid", 0, 0),
+            ChallengeProgression("testSenderUserUuid", 0, 0))
+
+    assertEquals("testUuid", challenge._uuid)
+    assertEquals(ChallengeType.REGULAR_STEP_CHALLENGE, challenge._type)
+    assertEquals(1000, challenge._stepsToMake)
+    assertEquals(10, challenge._kilometersToWalk)
+    assertEquals(7, challenge._daysToComplete)
+    assertEquals("2021-01-01", challenge._dateTime)
+    assertEquals("testChallengedUsername", challenge._challengedUsername)
+    assertEquals("testChallengedUserUuid", challenge._challengedUserUuid)
+    assertEquals("testSenderUsername", challenge._senderUsername)
+    assertEquals("testSenderUserUuid", challenge._senderUserUuid)
+  }
+
   @Test
   fun testCompletionFunctions() {
     var challenge =
