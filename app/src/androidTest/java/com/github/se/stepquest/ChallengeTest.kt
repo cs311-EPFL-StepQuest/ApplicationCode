11a072b8f4bc283bb2fdf26980d79fbbbb59fdaa
Raytimz <teplouhov.mitya@yandex.ru>
2024-05-24 10:50:34 +0200

    ktfmt

--- app/src/androidTest/java/com/github/se/stepquest/ChallengeTest.kt
+++ app/src/androidTest/java/com/github/se/stepquest/ChallengeTest.kt
@@ -6,11 +6,24 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ChallengeTest {
-  @Test fun testCompletionFunctions() {
-    var challenge = createChallengeItem("testUserId", "testUserName", "testUserId", "testUserName", ChallengeType.REGULAR_STEP_CHALLENGE)
+  @Test
+  fun testCompletionFunctions() {
+    var challenge =
+        createChallengeItem(
+            "testUserId",
+            "testUserName",
+            "testUserId",
+            "testUserName",
+            ChallengeType.REGULAR_STEP_CHALLENGE)
     var result = ChallengeType.REGULAR_STEP_CHALLENGE.completionFunction(challenge)
     assertEquals(false, result)
-    challenge = createChallengeItem("testUserId", "testUserName", "testUserId", "testUserName", ChallengeType.DAILY_STEP_CHALLENGE)
+    challenge =
+        createChallengeItem(
+            "testUserId",
+            "testUserName",
+            "testUserId",
+            "testUserName",
+            ChallengeType.DAILY_STEP_CHALLENGE)
     result = ChallengeType.DAILY_STEP_CHALLENGE.completionFunction(challenge)
     assertEquals(false, result)
   }
